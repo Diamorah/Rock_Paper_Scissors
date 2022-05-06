4d858649b541fec7a16d22eee8d35be94c124d5c
Dillion <dillion1991@hotmail.com>
2022-05-05 20:50:15 -0600

    Got RPS working in the browser console.

--- javascript.js
+++ javascript.js
@@ -1,25 +1,48 @@
+const options = ['rock','paper','scissors']
 
-let options = ["Rock", "Paper", "Scissors"]
+function computerPlay() {
+	let compChoice = options[options.length * Math.random() | 0]
+	return compChoice;
+}
 
-let computerPlay, 
-	compScore,
-	playerSelect,  
-	userScore;
+function playRound(playerSelection = prompt("Rock Paper Scissors"), computerSelection = computerPlay()) {
+	playerSelection = playerSelection.toUpperCase();
+	computerSelection = computerSelection.toUpperCase();
+	
+	console.log("You chose: " + playerSelection);
+	console.log("Your Opponent Chose " + computerSelection)
+	
+	if (computerSelection === playerSelection) {
+		console.log("It's A Draw!")
+	} else if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
+		console.log("You lose!");
+	} else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
+		console.log("You lose!");
+	} else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
+		console.log("You lose!");
+	} else if (computerSelection === "ROCK" && playerSelection === "PAPER") {
+		console.log("You Win!");
+	} else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
+		console.log("You Win!");
+	}  else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
+		console.log("You Win!");
+	} 
+}
 
-computerPlay = options[(Math.random() * options.length) | 0];
-
-console.log("Your opponent chose " + computerPlay);
+function playAgain() {
+	answer = prompt("Would you like to play again?: ")
+	if (answer == "y") {
+		playRound();
+	} else {
+		console.log("thanks for playing");
+	}
+}
 
 function game() {
-	playerSelect = prompt("Rock, Paper, or Scissors?");
-	if (playerSelect != options[0] || playerSelect != options[1] || playerSelect != options[2]) {
-		alert("You need to pick Rock, Paper, or Scissors");
-	} else if (playerSelect === options[0,1,2]) {
-		console.log("You chose " + playerSelect)
+	for (let i = 0; i < 5; i++) {
+		computerPlay();
+		playRound();
 	}
 
 }
-game();
-
-
-
+game()
